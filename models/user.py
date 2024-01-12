1c63d16454ede79e76e18823380881b516c77d5a
Adam Sanusi Babatunde <tundey520@gmail.com>
2024-01-12 23:58:06 +0100

    Users Security improvements (#17)
    * Review objects - RESTFul API methods
    
    * Place - Amenity: RESTful api methods
    
    * Users Security improvements

--- models/user.py
+++ models/user.py
@@ -2,10 +2,9 @@
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +26,4 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = md5(self.password.encode()).hexdigest()
